f8ff318b4159c9f31909e3031cb5ec140b4e6334
Adam <adam.seynar@yandex.ru>
2023-07-10 23:21:00 +0300

    some refactor

--- src/app.js
+++ src/app.js
@@ -1,27 +1,11 @@
-import Handlebars from 'handlebars/runtime';
-import LoginPage from './feature/Forms/Login/pages/LoginPage';
-
-import './styles/index.scss';
-import Button from './common/Button';
-import RegistrationPage from './feature/Forms/Registration/pages/RegistrationPage/RegistrationPage';
-import FiftyPage from './feature/Error/pages/FiftyPage/FiftyPage';
-import FourtyPage from './feature/Error/pages/FourtyPage/FourtyPage';
-import RowData from './feature/Profile/atoms/RowData';
-import ActionBtn from './feature/Profile/atoms/ActionBtn/ActionBtn';
-import About from './feature/Profile/molecules/About/About';
-import InfoPage from './feature/Profile/pages/Info';
-import ChangeDataPage from './feature/Profile/pages/ChangeData';
-import ChangePasswordPage from './feature/Profile/pages/ChangePassword';
-import ChatPage from './feature/Chat/pages/ChatPage';
-import ChatEmpty from './feature/Chat/pages/ChatEmpty';
 import { routes } from './consts/routes';
-
-// register partials
+import './styles/index.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = document.getElementById('app');
   const location = window.location.pathname;
-  app.innerHTML = routes[location]
+  const currentRoute = routes[location]
     ? routes[location]
     : routes['/fourty'];
+  app.innerHTML = currentRoute;
 });
