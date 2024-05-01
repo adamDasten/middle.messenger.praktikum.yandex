c575aebf9466e6384eb938e3d15e63463bab9c3a
Adam <adam.seynar@yandex.ru>
2024-05-01 16:16:28 +0300

    save changes

--- src/services/connect.ts
+++ src/services/connect.ts
@@ -4,10 +4,18 @@ import { Indexed } from "../types";
 import isEqual from "../utils/isEqual";
 import Store, { StoreEvents } from "./Store";
 import profileSvg from "../../static/profile_placeholder.svg";
-import {
-	ContentMessage,
-	ContentEmpty,
-} from "../feature/Chat/pages/MessagePage/MessagePage";
+
+import read from "#/static/read.svg";
+import optionsSvg from "#/static/options.svg";
+import addFile from "#/static/add_svg.svg";
+import arrowGo from "#/static/arrow_next.svg";
+import UsersList from "../components/UsersList";
+import UserItem from "../components/UserItem";
+import Message from "../components/Message";
+import FormDialog from "../components/FormDialog";
+import DialogInput from "../components/DialogInput";
+import Talking from "../components/Talking";
+import EmptyTalk from "../components/EmptyTalk";
 
 function connect(mapStateToProps: (state: Indexed) => Indexed) {
 	return function (Component: typeof Block) {
@@ -152,7 +160,62 @@ export const withChatItems: Function = connect((state) => {
 	};
 });
 
+// TODO: Сделать в будущем рефакторинг, сейчас нельзя тк если вынести компоненты - конфликт импортов
+
 export const withChatTemplate: Function = connect((state) => {
+	const UserListWrapper = withUsers(UsersList);
+
+	const usersList = new UserListWrapper({
+		items: Store.getState()?.userSearchRes as UserItem[],
+	});
+
+	const messages = [
+		new Message({
+			your: false,
+			message: `Привет! Смотри, тут всплыл интересный кусок лунной космической истории — НАСА 
+    в какой-то момент попросила Хассельблад адаптировать модель SWC для полетов на Луну. 
+    Сейчас мы все знаем 
+    что астронавты летали с моделью 500 EL — и к слову говоря, все тушки этих камер
+    все еще находятся на поверхности Луны, так как астронавты с собой забрали 
+    только кассеты с пленкой.
+    Хассельблад в итоге адаптировал SWC для космоса, но что-то пошло не так и на ракету они так 
+    никогда и не попали. 
+    Всего их было произведено 
+    25 штук, одну из них недавно продали на аукционе за 45000 евро.`,
+			time: "11:50",
+		}),
+		new Message({
+			your: "message--your",
+			read,
+			message: "Очень круто выглядит!",
+			time: "15:48",
+		}),
+	];
+
+	const form = new FormDialog({
+		addFile,
+		arrowGo,
+		input: new DialogInput({
+			attr: {
+				type: "text",
+				name: "message",
+				placeholder: "Сообщение",
+			},
+		}),
+	});
+
+	const ContentMessage = new Talking({
+		messages,
+		optionsSvg,
+		username: "Вадим",
+		form,
+		users: usersList,
+	});
+
+	const ContentEmpty = new EmptyTalk({
+		text: "Выберите чат чтобы отправить сообщение",
+	});
+
 	if (!state.currentChatId) {
 		return {};
 	}
